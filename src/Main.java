5c35ebff84674ff71f1249f5a650e67d8e087714
Vitalii Dolmatov <dolmatov-vs@yandex.ru>
2020-05-01 12:33:21 +0300

    init project

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int PriceInКopecks = 659999;
+        int Bonus = PriceInКopecks / 2000;
+        System.out.println(Bonus);
+    }
+}
